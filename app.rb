92bac5b3523946ffcc231f29980c71015adeb8f9
Chris <chris.haack@me.com>
2013-12-15 20:30:05 -0500

    remove sql and clean up comments

--- app.rb
+++ app.rb
@@ -28,9 +28,7 @@ module IrishEnglish
           session['id'] = 2
         end
         erb :quiz
-    end
-      
-  
+    end 
 
     post '/results' do
       @results = params["Q1"]
@@ -41,22 +39,4 @@ module IrishEnglish
 end
 
 
-# <% if session[:id].nil? %>
-#   <img class="quiz1" src= "<%= @images[0] %>">
-#   <img class="quiz1" src= "<%= @images[1] %>">  
-# <% else%>
-# <% session[:id] = 1 %>
-#   <% while session[:id] < 13 %>
-#   <img class="quiz1" src= "<%= @images[session[:id]] %>">
-#   <img class="quiz1" src= "<%= @images[session[:id] + 1] %>">
-#   <% session[:id]+= 1 %>
-#   <% end %>
-# <% end %>
-
-
-#     #helpers
-#     helpers do
-#       def partial(file_name)
-#         erb file_name, :layout => false
-#       end
 
--- lib/quiz.rb
+++ lib/quiz.rb
@@ -9,18 +9,3 @@ class Quiz < ActiveRecord::Base
  		end
 
 end
-
-#quiz = Quiz.new
-#p quiz.image.inspect
-#p Dir["../public/img/*"]
-
-
-# <% session[:id] = @images[1] %>
-# <% elsif session[:id] == @images[1] %>
-# <img class="quiz1" src= "<%= @images[2] %>">
-# <img class="quiz1" src= "<%= @images[3] %>">
-# <% session[:id] = @images[3] %>
-# <% elsif session[:id] == @images[3] 
-# <img class="quiz1" src= "<%= @images[4] %>">
-# <img class="quiz1" src= "<%= @images[5] %>">
-# <% session[:id] = @images[5] %>
